13e388be2c6016f87ec202ddadf5212ab5d8a6e7
futurGH <me@futuristick.ca>
2023-10-28 15:22:49 -0400

    Add memory, vector search, agent to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,30 @@
 import type { D1Database, VectorizeIndex } from "@cloudflare/workers-types";
 import { Type as T } from "@sinclair/typebox";
 import { Value } from "@sinclair/typebox/value";
+import { BaseClient as XataClient } from "@xata.io/client";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { streamSSE } from "hono/streaming";
 import { validator } from "hono/validator";
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { CloudflareWorkersAIEmbeddings } from "langchain/embeddings/cloudflare_workersai";
+import { BufferMemory } from "langchain/memory";
+import { XataChatMessageHistory } from "langchain/stores/message/xata";
+import { DynamicTool } from "langchain/tools";
+import { formatDocumentsAsString } from "langchain/util/document";
+import { XataVectorSearch } from "langchain/vectorstores/xata";
 
 export type Env = {
 	DB: D1Database;
 	COURSES_INDEX: VectorizeIndex;
 	PROGRAMS_INDEX: VectorizeIndex;
+	AI: Fetcher;
 
 	OPENAI_KEY: string;
 	RLIMIT_NAMESPACE: string;
+	XATA_API_KEY: string;
+	XATA_DB_URL: string;
 };
 
 const app = new Hono<{ Bindings: Env }>();
@@ -30,12 +42,12 @@ app.post(
 			: c.text("Invalid request parameters", 400);
 	}),
 	async (c, next) => {
-		// const ip = c.req.header("cf-connecting-ip");
-		// if (!ip) {
-		// 	return c.text("Invalid request", 400);
-		// }
-		// const limit = await fetch(`https://rlimit.com/${c.env.RLIMIT_NAMESPACE}/50/1h/${ip}`);
-		// if (!limit.ok) return c.text("Rate limit exceeded", 429);
+		const ip = c.req.header("cf-connecting-ip");
+		if (!ip) {
+			return c.text("Invalid request", 400);
+		}
+		const limit = await fetch(`https://rlimit.com/${c.env.RLIMIT_NAMESPACE}/50/1h/${ip}`);
+		if (!limit.ok) return c.json(await limit.json(), 429);
 		return next();
 	},
 	validator("form", (value, c) => {
@@ -45,19 +57,117 @@ app.post(
 		const { id } = c.req.valid("param");
 		const { message, schoolName } = c.req.valid("form");
 
-		console.log(message, schoolName, id);
-		return streamSSE(c, async (stream) => {
-			const test =
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales ipsum ac nunc dictum, rhoncus malesuada risus congue. Donec bibendum, sapien id consectetur efficitur, massa metus tincidunt nibh, ac dictum sem orci vitae felis. Pellentesque vel tempus felis. Aliquam erat volutpat. Maecenas nec condimentum ante. Suspendisse potenti. Aenean mauris nisi, elementum a sodales eu, iaculis eget odio. Sed pulvinar nisi a interdum cursus. In tortor purus, malesuada et tincidunt id, ornare vel metus."
-					.split(" ").map((s) => s + " ");
-			for (const word of test) {
-				console.log(word);
-				const random = Math.random() * 500;
-				await stream.sleep(random);
-				await stream.write(`event: token\ndata: ${encodeURIComponent(word)}\n\n`);
-			}
-			await stream.write(`event: end\n\n`);
-			await stream.close();
+		const embeddings = new CloudflareWorkersAIEmbeddings({
+			binding: c.env.AI,
+			modelName: "@cf/baai/bge-base-en-v1.5",
+		});
+
+		const xata = new XataClient({
+			apiKey: c.env.XATA_API_KEY,
+			databaseURL: c.env.XATA_DB_URL,
+			branch: "main",
+		});
+		const coursesStore = new XataVectorSearch(embeddings, { client: xata, table: "courses" });
+		const programsStore = new XataVectorSearch(embeddings, { client: xata, table: "programs" });
+
+		const chatHistory = new XataChatMessageHistory({
+			createTable: true,
+			table: "messages",
+			sessionId: id,
+			client: xata,
+			apiKey: c.env.XATA_API_KEY,
+		});
+
+		const memory = new BufferMemory({
+			returnMessages: true,
+			chatHistory,
+			memoryKey: "chat_history",
+		});
+
+		const llm = new ChatOpenAI({
+			openAIApiKey: c.env.OPENAI_KEY,
+			modelName: "gpt-4",
+			temperature: 0.9,
+			streaming: true,
+			verbose: true,
+		});
+
+		const coursesRetriever = coursesStore.asRetriever({ filter: { schoolName } });
+		const coursesRetrieverTool = new DynamicTool({
+			name: "search_courses",
+			description: `Useful for information on courses at ${schoolName}.`,
+			func: async (input, runManager) => {
+				const docs = await coursesRetriever.getRelevantDocuments(
+					input,
+					runManager?.getChild("retriever"),
+				);
+				return formatDocumentsAsString(docs, "\n");
+			},
+		});
+
+		const programsRetriever = programsStore.asRetriever();
+		const programsRetrieverTool = new DynamicTool({
+			name: "search_programs",
+			description: "Useful for information on university/college programs.",
+			func: async (input, runManager) => {
+				const docs = await programsRetriever.getRelevantDocuments(
+					input,
+					runManager?.getChild("retriever"),
+				);
+				return formatDocumentsAsString(docs, "\n");
+			},
+		});
+
+		const agent = await initializeAgentExecutorWithOptions(
+			[programsRetrieverTool, coursesRetrieverTool],
+			llm,
+			{
+				memory,
+				agentType: "openai-functions",
+				agentArgs: {
+					prefix:
+						`Acting as a guidance counselor for ${schoolName}, your job is to guide students through their academic and career choices using your extensive knowledge of the school’s course offerings and all Ontario college and university programs. Refer to specific high school courses by both their name and their code whenever possible. Make sure to use functions wherever possible for accurate data. Answer questions to the fullest of your knowledge. Be concise, aiming for 1-2 brief paragraphs in your response.`,
+				},
+				verbose: true,
+				maxIterations: 12,
+				earlyStoppingMethod: "generate",
+				handleParsingErrors:
+					"Try wording your query to the tool differently and try again.",
+			},
+		);
+
+		return streamSSE(c, async (sseStream) => {
+			await agent.stream({ input: message }, {
+				callbacks: [{
+					handleLLMNewToken: async (token) => {
+						if (!token.length) return;
+						await sseStream.write(
+							`event: token\ndata: ${encodeURIComponent(token)}\n\n`,
+						);
+					},
+					handleAgentEnd: async (action) => {
+						let end = `event: end\n`;
+						if ("output" in action.returnValues) {
+							end += `data: ${encodeURIComponent(`${action.returnValues.output}`)}\n`;
+						}
+						end += `\n`;
+						await sseStream.write(end);
+						await sseStream.close();
+					},
+					handleAgentAction: async (action) => {
+						await sseStream.write(
+							`event: action\ndata: ${encodeURIComponent(action.tool)}\n\n`,
+						);
+					},
+					handleChainError: async (err) => {
+						console.error(err);
+						await sseStream.write(
+							`event: error\ndata: ${encodeURIComponent(`${err}`)}\n\n`,
+						);
+						await sseStream.close();
+					},
+				}],
+			});
 		});
 	},
 );
